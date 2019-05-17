f8b6fd19f04629ef3f0939264b7683126beca387
Jake Hageman <jacob.j.hageman@nasa.gov>
2019-05-17 15:07:57 -0400

    Seed from gen_msgids 1.0 code base

---
+++ gen_msgids.h
@@ -0,0 +1,51 @@
+/*
+**      GSC-18128-1, "Core Flight Executive Version 6.6"
+**
+**      Copyright (c) 2006-2019 United States Government as represented by
+**      the Administrator of the National Aeronautics and Space Administration.
+**      All Rights Reserved.
+**
+**      Licensed under the Apache License, Version 2.0 (the "License");
+**      you may not use this file except in compliance with the License.
+**      You may obtain a copy of the License at
+**
+**        http://www.apache.org/licenses/LICENSE-2.0
+**
+**      Unless required by applicable law or agreed to in writing, software
+**      distributed under the License is distributed on an "AS IS" BASIS,
+**      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**      See the License for the specific language governing permissions and
+**      limitations under the License.
+**
+** Purpose:  
+**
+** Author:  
+**
+** Notes:
+**
+** $Log: gen_msgids.h  $
+** Revision 1.2 2012/01/13 13:37:09GMT-05:00 acudmore 
+** Added license/copyright text
+** Revision 1.1 2008/11/19 08:51:02EST wfmoleski 
+** Initial revision
+** Member added to project c:/MKSDATA/MKS-REPOSITORY/MKS-CFE-PROJECT/fsw/build/cpu1/gen_msgids/project.pj
+**
+*/
+/*************************************************************************/
+
+/*
+** Ensure that header is included only once...
+*/
+#ifndef _tst_msgids_
+#define _tst_msgids_
+
+/*
+** Required header files...
+*/
+
+#include "app_mission_cfg.h"
+#include "app_msgids.h"
+#include "cfe_mission_cfg.h"
+#include "cfe_msgids.h"
+
+#endif /* _gen_msgids_ */
